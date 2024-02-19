8eb055e54c92452871981bec8a3f7c35ebf614bd
Aamir Saleem <65162736+aamirsaleem1286@users.noreply.github.com>
2024-02-19 06:40:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ require("./config/db");
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  origin:["https://deploy-mern-1whq.vercel.app"],
+methods:["POST", "GET"],
+  credentials:true
+));
 
 app.use(express.json());
 
